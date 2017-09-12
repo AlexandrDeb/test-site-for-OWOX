96351f2d97810cb177fc3523819ed62a00242574
AlexandrDeb <alexanderpgs@ukr.net>
2017-09-12 15:05:54 +0300

    final fixing

--- views/pages/index.php
+++ views/pages/index.php
@@ -1,4 +1,5 @@
 <?php if (Session::get('login')) { ?>
+    
     <h2>Добро пожаловать, <?= Session::get('login') ?></h2>
     <div><?= Session::get('passwordFb') ?></div>
     <div><?= Session::get('emailFb') ?></div>
--- views/privats/index.php
+++ views/privats/index.php
@@ -1,8 +1,6 @@
 <?php
 if (!Session::get('login')) {
     header("Location: /");
-    
-
 }
 
 ?>
